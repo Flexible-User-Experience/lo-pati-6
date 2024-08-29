65c1a761314de055cc3c08b51b9a459064b374a7
David Romaní <david@flux.cat>
2024-08-29 17:21:18 +0200

    fix missing icheck-bootstrap CSS problem

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
 import './bootstrap.js';
 import { Tooltip } from 'bootstrap';
 
+import('icheck-bootstrap/icheck-bootstrap.min.css');
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle-second="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
